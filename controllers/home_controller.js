233f562d564d147fc39173bad16b12c8a866a50c
AbhishekJha1916 <akjha418@gmail.com>
2023-05-26 13:45:13 +0530

    v0.8.0 - Minor Update - Divided layout into partials and bug fixes in navbar

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@
 
 module.exports.home = function (request, response) {
   console.log("Home controller action is working");
-  return response.render("homeview", {
+  return response.render("home", {
     title: "Home View",
   });
 };
