7f0dddc99d3766f3ff7763d0b0bb9e83e6ca4699
Om Sinkar <omsinkar03bit@gmail.com>
2023-06-20 22:29:27 +0530

    v0.8.5: Authentication error solved

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const sassMiddleware = require("node-sass-middleware");
+const db = require('./config/mongoose');
 
 console.log("middleware before");
 // using the sass middleware
@@ -15,21 +16,34 @@ app.use(
     prefix: "/css",
   })
 );
-console.log("middleware after");
-// use assets directory for static files via middleware
-app.use(express.static("./assets"));
+// console.log("middleware after");
+// // use assets directory for static files via middleware
+// // app.use(express.urlencoded());
+// // app.use(express.static("./assets"));
 
-// use express layouts via middleware
+// // use express layouts via middleware
+// app.use(expressLayouts);
+// app.set("layout extractStyles", true);
+// app.set("layout extractScripts", true);
+
+// // use express router
+// app.use("/", require("./routes/home.js"));
+
+// // use ejs as the view engine
+// app.set("view engine", "ejs");
+// app.set("views", "./views");
+// app.use(express.urlencoded());
+// app.use(express.static('./assets'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded());
+app.use(express.static('./assets'));
+
+app.set('layout extractStyles', true);
+app.set('layout extractScript', true);
+
 app.use(expressLayouts);
-app.set("layout extractStyles", true);
-app.set("layout extractScripts", true);
-
-// use express router
-app.use("/", require("./routes/home.js"));
-
-// use ejs as the view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.use('/', require('./routes/home.js'));
 
 app.listen(port, function (error) {
   if (error) {
