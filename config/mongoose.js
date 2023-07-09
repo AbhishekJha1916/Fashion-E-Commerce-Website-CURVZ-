4e1dee4edf99a185ea2775cda2dd2450d5543131
AbhishekJha1916 <akjha418@gmail.com>
2023-07-09 14:34:20 +0530

    MongoDB Atlas Added - vNot Required

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Curvz");
+// mongoose.connect("mongodb://localhost:27017/Curvz");
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to MongoDB."));
+// db.on("error", console.error.bind(console, "Error connecting to MongoDB."));
 
-db.once("open", function () {
-  console.log("Connected to Database :: MongoDB.");
-});
+// db.once("open", function () {
+//   console.log("Connected to Database :: MongoDB.");
+// });
 
-module.exports = db;
+// module.exports = db;
+
+const uri = "mongodb+srv://akjha418:hVOIwwpO1Z0fqq3g@curvzcluster.3t1enqi.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
+
+const start = async() => {
+    try{
+        await mongoose.connect(uri);
+
+    }
+    catch(err){
+        console.log(err); 
+    }
+
+}
+start()
